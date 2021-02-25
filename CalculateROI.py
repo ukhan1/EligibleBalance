76180bb0acb46950f9c062aee1dc220c693e489d
ukhan1 <usamakhan015@yahoo.com>
2021-02-24 16:51:43 -0800

    Expanded Error Coverage
    -AP strings are not case-sensitive
    -Added parameter names to table columns
    -Added error recognition for incorrect values and unexpected errors

--- CalculateROI.py
+++ CalculateROI.py
@@ -2,7 +2,7 @@
 """
 Created on Fri Feb 19 15:54:45 2021
 
-@author: playe
+@author: Usama
 """
 
 import os
@@ -15,9 +15,9 @@ from openpyxl import Workbook, load_workbook
 from openpyxl.styles import Border, Side, PatternFill, Font, GradientFill, Alignment
 
 declared_roi = 1.5
-issued = "AP Letter Issued"
-cancelled = "AP Letter Cancelled"
-purchased = "Home Purchased"
+issued = "ap letter issued"
+cancelled = "ap letter cancelled"
+purchased = "home purchased"
 
 #######################
 ### Get directories ###
@@ -36,6 +36,7 @@ error_wb = Workbook()
 error_ws = error_wb.active
 principal_wb = Workbook()
 principal_ws = principal_wb.active
+
 dir_list = sorted(os.listdir(dir_pre))
 print(dir)
 ##################################
@@ -89,6 +90,7 @@ print(end_of_quarter)
 #######################
 ### File processing ###
 #######################
+
 COUNT = 1
 def increment():
     global COUNT
@@ -131,14 +133,14 @@ def partial_roi(ws, file, start_of_m, end_of_m, i, ap):
                 continue
             ### must continue until we reach m
             if(next_date <= start_of_m):
-                if(issued in ws.cell(row = j, column = 2).value):
-                    if((ws.cell(row = j, column = 1).value) < (start_of_m - timedelta(days = 90)).date()):
+                minimum = ws.cell(row = j, column = 7).value
+                if(issued in ws.cell(row = j, column = 2).value.lower()):
+                    if((ws.cell(row = j, column = 1).value) >= (start_of_m - timedelta(days = 90)).date()):
                         print("issued")
                         ap_issued = 1
-                elif((cancelled in ws.cell(row = j, column = 2).value) | (purchased in ws.cell(row = j, column = 2).value)):
+                elif((cancelled in ws.cell(row = j, column = 2).value.lower()) | (purchased in ws.cell(row = j, column = 2).value.lower())):
                     print("cancelled")
                     ap_issued = 0
-                minimum = ws.cell(row = j, column = 7).value
                 i=j
                 j+=1
                 continue
@@ -147,11 +149,11 @@ def partial_roi(ws, file, start_of_m, end_of_m, i, ap):
                 if(minimum > ws.cell(row = j, column = 7).value):
                     minimum = ws.cell(row = j, column = 7).value
                     i = j
-                if(issued in ws.cell(row = j, column = 2).value):
+                if(issued in ws.cell(row = j, column = 2).value.lower()):
                     print("ap issued")
                     ap_issued = 1
                     return minimum, 0, i, 0, ap_issued
-                elif((cancelled in ws.cell(row = j, column = 2).value) | (purchased in ws.cell(row = j, column = 2).value)):
+                elif((cancelled in ws.cell(row = j, column = 2).value.lower()) | (purchased in ws.cell(row = j, column = 2).value.lower())):
                     print("ap_cancelled")
                     ap_issued = 0
                     return 0, 0, i, 0, ap_issued
@@ -173,6 +175,7 @@ def partial_roi(ws, file, start_of_m, end_of_m, i, ap):
                         error_ws.cell(row = COUNT, column = 2).value = "Missing Dates"
                         increment()
                         error_wb.save(dir_error)
+                        return minimum, 1, i, 0, ap_issued
                 j+=1
                 continue
             elif(ws.cell(row = j+2, column = 7).value != None):
@@ -184,23 +187,30 @@ def partial_roi(ws, file, start_of_m, end_of_m, i, ap):
                         error_ws.cell(row = COUNT, column = 2).value = "Missing Dates"
                         increment()
                         error_wb.save(dir_error)
+                        return minimum, 1, i, 0, ap_issued
                 j+=2
                 continue
             ### missing dates should be somewhat frequent in the beginning
             else:
                 return minimum, 0, i, 1, ap_issued
-            
+        except TypeError:
+            error_ws.cell(row = COUNT, column = 1).value = file 
+            error_ws.cell(row = COUNT, column = 2).value = "Other Error"
+            increment()
+            error_wb.save(dir_error)
+            return 0,1,i,0,ap_issued
     
     print("Done partial roi")
     # return{'partial_roi' : minimum, 'error' : 0, 'r' : i, 'EoF' : 0}
-    return minimum, 0, i, 0
+    return minimum, 0, i, 0, ap_issued
 
-def write_principal_column(file, p1, p2, p3, roi):
+def write_principal_column(file, p1, p2, p3, avg, roi):
     principal_ws.cell(row = p_COUNT, column = 1).value = file
     principal_ws.cell(row = p_COUNT, column = 2).value = p1
     principal_ws.cell(row = p_COUNT, column = 3).value = p2
     principal_ws.cell(row = p_COUNT, column = 4).value = p3
-    principal_ws.cell(row = p_COUNT, column = 5).value = roi
+    principal_ws.cell(row = p_COUNT, column = 5).value = avg
+    principal_ws.cell(row = p_COUNT, column = 6).value = roi
     principal_wb.save(dir_principal)
 def process_file(infile, outfile, file):
     out_of_range = 0
@@ -224,53 +234,75 @@ def process_file(infile, outfile, file):
             increment()
             error_wb.save(dir_error)
             return
-    except :
-        print("Missing date in first row")
-        error_ws.cell(row = COUNT, column = 1).value = file 
-        error_ws.cell(row = COUNT, column = 2).value = "Missing first row"
-        increment()
-        error_wb.save(dir_error)
-        return
-    
-    
-    p1, error, r, EoF, ap = partial_roi(ws, file, start_of_m1, end_of_m1, r, 0)
-    if(error):
-        print("error: check log file")
-        return
-    if(ap):
-        p1 = 0
-    if(EoF):
-        p2 = p1
-        p3 = p1
-    else:
-        print("passing in row", r, " to p2")
-        p2, error, r, EoF, ap = partial_roi(ws, file, start_of_m2, end_of_m2, r, ap)
+        p1, error, r, EoF, ap = partial_roi(ws, file, start_of_m1, end_of_m1, r, 0)
         if(error):
             print("error: check log file")
             return
-    if(ap):
-        p2 = 0
-    if(EoF):
-        p3 = p2
-    else:
-        p3, error, r, EoF, ap = partial_roi(ws, file, start_of_m3, end_of_m3, r, ap)
-        if(error):
-            print("error: check log file")
-            return
-    if(ap):
-        p3 = 0
-    print("P1:",p1)
-    print("P2:",p2)
-    print("P3:",p3)
-    roi = (p1 + p2 + p3) * declared_roi/(3*100)
-    write_principal_column(file, p1, p2, p3, roi)
-    p_increment()
+        if(ap):
+            p1 = 0
+        if(EoF):
+            p2 = p1
+            p3 = p1
+        else:
+            print("passing in row", r, " to p2")
+            p2, error, r, EoF, ap = partial_roi(ws, file, start_of_m2, end_of_m2, r, ap)
+            if(error):
+                print("error: check log file")
+                return
+        if(ap):
+            p2 = 0
+        if(EoF):
+            p3 = p2
+        else:
+            p3, error, r, EoF, ap = partial_roi(ws, file, start_of_m3, end_of_m3, r, ap)
+            if(error):
+                print("error: check log file")
+                return
+        if(ap):
+            p3 = 0     
+        print("P1:",p1)
+        print("P2:",p2)
+        print("P3:",p3)
+        avg = (p1 + p2 + p3)/3
+        roi = (p1 + p2 + p3) * declared_roi/(3*100)
+        write_principal_column(file, p1, p2, p3, avg, roi)
+        p_increment()
+    except AttributeError:
+       print("Missing date in first row")
+       error_ws.cell(row = COUNT, column = 1).value = file 
+       error_ws.cell(row = COUNT, column = 2).value = "Missing first row"
+       increment()
+       error_wb.save(dir_error)
+       book.close()
+       return
+    except TypeError:
+       print("Possible incorrect value")
+       error_ws.cell(row = COUNT, column = 1).value = file 
+       error_ws.cell(row = COUNT, column = 2).value = "Incorrect value"
+       increment()
+       error_wb.save(dir_error)
+       book.close()
+       return
+    except:
+       print("Unexpected error")
+       error_ws.cell(row = COUNT, column = 1).value = file 
+       error_ws.cell(row = COUNT, column = 2).value = "Unexpected error"
+       increment()
+       error_wb.save(dir_error)
+       book.close() 
+       return
     book.close()
     
-    
+principal_ws.cell(row =  1, column = 1).value = "File No"
+principal_ws.cell(row =  1, column = 2).value = "P1"
+principal_ws.cell(row =  1, column = 3).value = "P2"
+principal_ws.cell(row =  1, column = 4).value = "P3"
+principal_ws.cell(row =  1, column = 5).value = "Avg"
+principal_ws.cell(row =  1, column = 6).value = "ROI"
+p_increment()
 for file in sorted( filter(lambda x: not (x.startswith('~') or x.startswith('.')), dir_list) ):
 #file = "0733C2.xlsx"
-    file_in = os.path.join(dir_pre, "0944-HO-Error.xlsx")
+    file_in = os.path.join(dir_pre, file)
     file_out = os.path.join(dir_post, file)
     #file_in = file_in.replace(os.sep, '/')
     print(file_in)
